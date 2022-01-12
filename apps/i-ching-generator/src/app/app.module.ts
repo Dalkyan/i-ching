0c596319cc033441cca58c6696a53ba7e4519f12
Dalkyan <36706161+Dalkyan@users.noreply.github.com>
2022-01-12 22:33:53 +0100

    Foundation for layout and logic

--- apps/i-ching-generator/src/app/app.module.ts
+++ apps/i-ching-generator/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { HomeScreenComponent } from './home-screen/home-screen.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent, NxWelcomeComponent, HomeScreenComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
